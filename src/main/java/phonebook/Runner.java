97e175b44bfb38c0a6d17317e308edce02270323
ListFroker <snackbags05@gmail.com>
2020-01-23 00:20:49 +0300

    checking ids (alpha)

--- src/main/java/phonebook/Runner.java
+++ src/main/java/phonebook/Runner.java
@@ -30,11 +30,15 @@ public class Runner {
                     ConsoleDisplay.printContacts(phoneBook.getAllContacts());
 
                 } else if (input == ActionType.SHOW.getIndex()) {
-                    System.out.println("Enter id: ");
-                    int id = scanner.nextInt();
-                    Contact contact = phoneBook.find(id);
-                    ConsoleDisplay.printContact(contact);
-
+                    while (true) {
+                        System.out.println("Enter id: ");
+                        int id = scanner.nextInt();
+                        if(scanner.hasNextInt()) {
+                            Contact contact = phoneBook.find(id);
+                            ConsoleDisplay.printContact(contact);
+                        }
+                        break;
+                    }
                 } else if (input == ActionType.DELETE.getIndex()) {
                     System.out.println("Enter id: ");
                     int id = scanner.nextInt();
@@ -42,28 +46,31 @@ public class Runner {
 
                 } else if (input == ActionType.FIND.getIndex()) {
                     while (true) {
-                        System.out.println("You can find by name or by phone( 1 - by name, 2- by phone)");
+                        System.out.println("You can find by name or by phone( 1 - by full name, 2- by part of name, 3 - by last 4 numbers of phone)");
+                        scanner.next();
+                        if(scanner.hasNextInt()) {
+                            int findId = scanner.nextInt();
+                            if (findId == 2) {
+                                System.out.println("Find by first letters of name:");
+                                String name = scanner.next();
+                                ArrayList<Contact> filteredContacts = phoneBook.findByNameStart(name);
+                                ConsoleDisplay.printContacts(filteredContacts);
+                            } else if (findId == 3) {
+                                System.out.println("Find by last 4 numbers of phone: ");
+                                String phone = scanner.next();
+                                ArrayList<Contact> filteredContacts = phoneBook.findByPhone(phone);
+                                ConsoleDisplay.printContacts(filteredContacts);
+                            } else if (findId == 1) {
+                                System.out.println("Find by full name:");
+                                String fullName = scanner.next();
+                                ArrayList<Contact> filteredContacts = phoneBook.findByName(fullName);
+                                ConsoleDisplay.printContacts(filteredContacts);
+                            }
 
-                        int findId = scanner.nextInt();
-
-
-                        if (findId == 2) {
-                            System.out.println("Find by first letters of name:");
-                            String name = scanner.next();
-                            ArrayList<Contact> filteredContacts = phoneBook.findByNameStart(name);
-                            ConsoleDisplay.printContacts(filteredContacts);
-                        } else if (findId == 3) {
-                            System.out.println("Find by last 4 numbers of phone: ");
-                            String phone = scanner.next();
-                            ArrayList<Contact> filteredContacts = phoneBook.findByPhone(phone);
-                            ConsoleDisplay.printContacts(filteredContacts);
-                        } else if (findId == 1) {
-                            System.out.println("Find by full name:");
-                            String fullName = scanner.next();
-                            ArrayList<Contact> filteredContacts = phoneBook.findByName(fullName);
-                            ConsoleDisplay.printContacts(filteredContacts);
                         }
+
                     }
+
                 } else if (input == ActionType.SAVE.getIndex()) {
                     FileController.saveToFile(phoneBook.getAllContacts());
                     ;
@@ -75,20 +82,26 @@ public class Runner {
                         ConsoleDisplay.printContact(contact);
                         while(true) {
                             System.out.println("What you want to update?(1 - name, 2 - phone, 3 - birthday, 0 - exit)");
-                            int updateId = scanner.nextInt();
-
-                            if (updateId == 0) {
-                                break;
-                            } else if (updateId == 1) {
-                                contact.setName(scanner.next());
-                                ConsoleDisplay.printContact(contact);
-                            } else if (updateId == 2) {
-                                contact.setPhone(scanner.next());
-                                ConsoleDisplay.printContact(contact);
-                            } else if (updateId == 3) {
-                                contact.setBirthday(scanner.next());
-                                ConsoleDisplay.printContact(contact);
+                            scanner.next();
+                            if(scanner.hasNextInt()) {
+                                int updateId = scanner.nextInt();
+                                if (updateId == 0) {
+                                    break;
+                                } else if (updateId == 1) {
+                                    System.out.println("Enter new name:");
+                                    contact.setName(scanner.next());
+                                    ConsoleDisplay.printContact(contact);
+                                } else if (updateId == 2) {
+                                    System.out.println("Enter new phone:");
+                                    contact.setPhone(scanner.next());
+                                    ConsoleDisplay.printContact(contact);
+                                } else if (updateId == 3) {
+                                    System.out.println("Enter new birthday: ");
+                                    contact.setBirthday(scanner.next());
+                                    ConsoleDisplay.printContact(contact);
+                                }
                             }
+
                         }
                 }
             } else {
